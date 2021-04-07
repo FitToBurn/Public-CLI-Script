a5575842d7f429704c1f5288da83aac934ca3011
729376442 <55039866+729376442@users.noreply.github.com>
2021-04-07 16:25:59 +0800

    Update

---
+++ Synology Certificate.sh
@@ -0,0 +1,30 @@
+# Certificate installation script for Synology NAS.
+# Please disable http2 in Synology settings.
+# Command for auto renew task/generate renew logs:
+# sh /path/to/script.sh >> /path/to/renewlog.txt 2>&1
+cd ~
+wget https://github.com/acmesh-official/acme.sh/archive/master.tar.gz
+tar xvf master.tar.gz
+cd acme.sh-master/
+./acme.sh --install --nocron --home /usr/local/share/acme.sh --accountemail "[Cloudflare Email]"
+source ~/.profile
+export CF_Email="[Cloudflare Email]"
+export CF_Key="[CF Global API Key]"
+cd /usr/local/share/acme.sh
+export CERT_DOMAIN="*.example.com"
+export CERT_DNS="dns_cf"
+./acme.sh --issue --home . -d "$CERT_DOMAIN" --dns "$CERT_DNS"
+cd /usr/local/share/acme.sh
+export SYNO_Username='[Synology Username]'
+export SYNO_Password='[Password]'
+export SYNO_Scheme="https"
+export SYNO_Port="[https port]"
+export SYNO_DID=[2-factor auth cookie]
+export SYNO_Certificate="[Certificate Description]"
+./acme.sh --insecure --deploy --home . -d "$CERT_DOMAIN" --deploy-hook synology_dsm
+cp -f /usr/local/share/acme.sh//"*.example.com"/"fullchain.cer" /path/to/savefolder/
+cp -f /usr/local/share/acme.sh//"*.example.com"/"*.example.com.key" /path/to/savefolder/
+cd /path/to/savefolder/
+mv "*.example.com.key" privkey.key
+
+synoservice -restart pkgctl-Docker
---
+++ Synology DDNS.sh
@@ -0,0 +1,15 @@
+# Cloudflare DDNS script for Synology NAS.
+# DDNS setting in Synology:
+# Server Name: Domain
+# Username/Email: CF Region ID
+# Password: API token with DNS writing permission
+
+wget https://files.ioiox.com/GitHub/joshuaavalon/SynologyCloudflareDDNS/master/cloudflareddns.sh -O /sbin/cloudflareddns.sh
+chmod +x /sbin/cloudflareddns.sh
+cat >> /etc.defaults/ddns_provider.conf << EOF
+[Cloudflare]
+        modulepath=/sbin/cloudflareddns.sh
+        queryurl=https://www.cloudflare.com
+        website=https://www.cloudflare.com
+EOF
+vi /sbin/cloudflareddns.sh
