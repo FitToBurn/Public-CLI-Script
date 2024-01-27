81d62615d1a5447bcc8c5ee5a6771d47987a14a4
ShinonomeRyoko <55039866+729376442@users.noreply.github.com>
2024-01-27 04:47:41 -0800

    Update Synology DDNS.sh

--- Synology DDNS.sh
+++ Synology DDNS.sh
@@ -4,7 +4,7 @@
 # Username/Email: CF Region ID
 # Password: API token with DNS writing permission
 
-wget https://files.ioiox.com/GitHub/joshuaavalon/SynologyCloudflareDDNS/master/cloudflareddns.sh -O /sbin/cloudflareddns.sh
+wget https://raw.githubusercontent.com/joshuaavalon/SynologyCloudflareDDNS/master/cloudflareddns.sh -O /sbin/cloudflareddns.sh
 chmod +x /sbin/cloudflareddns.sh
 cat >> /etc.defaults/ddns_provider.conf << EOF
 [Cloudflare]
